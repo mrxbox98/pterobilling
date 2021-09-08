ad97cac92b5a81506ceac8598633b3fa36eb3eb3
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2021-09-08 15:52:56 -0700

    Update StripeWorker.ts

--- extensions/StripeWorker.ts
+++ extensions/StripeWorker.ts
@@ -1,10 +1,27 @@
 const stripe = require('stripe')('sk_test_51JIOoHCGRmSKq6LqDnK5KcIqCDRPQ8WFmuNsk5gpTaStYSQBwoujfqg9FN3EbH7ntZ2JmBwHlgLTehvxaleiSUSV00ePc7MG0k');
 
+/**
+ * Creates a new customer
+ * @param email The email to use with the customer
+ * @returns the customer object
+ */
 async function createCustomer(email:string) {
     const customer = await stripe.customers.create({
-        description: 'My First Test Customer (created for API docs)',
         email: email
     });
-
     return customer
+}
+
+/**
+ * Creates new subscription with the customer and the price
+ * @param customer The customer id
+ * @param itemPrice The price id
+ */
+async function createSubscription(customer:string, itemPrice:string) {
+    const subscription = await stripe.subscriptions.create({
+        customer: customer,
+        items: [
+          {price: itemPrice},
+        ],
+      });
 }
